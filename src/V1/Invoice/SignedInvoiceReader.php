22a2cd0c833547ecbd3e0405f107d465c6b87462
Fabio Soggia <f.soggia@gmail.com>
2018-08-31 18:00:13 +0200

    Aggiunto supporto a XAdES-BES.

--- src/V1/Invoice/SignedInvoiceReader.php
+++ src/V1/Invoice/SignedInvoiceReader.php
@@ -5,9 +5,6 @@ namespace CloudFinance\EFattureWsClient\V1\Invoice;
 use CloudFinance\EFattureWsClient\V1\Invoice\InvoiceData;
 use CloudFinance\EFattureWsClient\Exceptions\EFattureWsClientException;
 
-define("SIGNING_METHOD_CADES_BES", "CAdES-BES");
-define("SIGNING_METHOD_XADES_BES", "XAdES-BES");
-
 class SignedInvoiceReader
 {
     private $filePlainContent;
@@ -19,6 +16,9 @@ class SignedInvoiceReader
     private $signingMethod;
     private $invoiceData;
 
+    public const CAdES_BES = "CAdES-BES";
+    public const XAdES_BES = "XAdES-BES";
+
     private function __construct() {
     }
 
@@ -27,65 +27,75 @@ class SignedInvoiceReader
         $fileName = \trim($fileName);
 
         if (preg_match('/.xml.p7m$/i', $fileName) === 1) {
-            return SIGNING_METHOD_CADES_BES;
+            return self::CAdES_BES;
         }
         if (preg_match('/.xml$/i', $fileName) === 1) {
-            return SIGNING_METHOD_XADES_BES;
+            return self::XAdES_BES;
         }
         return false;
     }
 
     public static function getFileExtensionBySigningMethod(string $signingMethod)
     {
-        if ($signingMethod === SIGNING_METHOD_CADES_BES) {
+        if ($signingMethod === self::CAdES_BES) {
             return "xml.p7m";
         }
-        if ($signingMethod === SIGNING_METHOD_XADES_BES) {
+        if ($signingMethod === self::XAdES_BES) {
             return "xml";
         }
         return false;
     }
 
+    public static function removeXadESBESSignature(string $content)
+    {
+        $domDocument = new \DomDocument();
+        $domDocument->loadXML($content);
+        $nodes = $domDocument->getElementsByTagNameNS("http://www.w3.org/2000/09/xmldsig#", "Signature");
+        foreach ($nodes as $node) {
+            $node->parentNode->removeChild($node);
+        }
+        return $domDocument->saveXML();
+    }
+
     public static function createFromSignedString($signingMethod, $string)
     {
-        if (!in_array($signingMethod, [ SIGNING_METHOD_CADES_BES, SIGNING_METHOD_XADES_BES ])) {
+        if (!in_array($signingMethod, [ self::CAdES_BES, self::XAdES_BES ])) {
             throw new EFattureWsClientException("Field 'signingMethod' must be 'CAdES-BES' or 'XAdES-BES'.");
         }
 
         $invoice = new self;
 
-        $fileSignedPath = \tempnam(\sys_get_temp_dir(), "fsp");
-        $filePlainPath = \tempnam(\sys_get_temp_dir(), "fpp");
+        if ($signingMethod === self::CAdES_BES) {
+            $fileSignedPath = \tempnam(\sys_get_temp_dir(), "fsp");
+            $filePlainPath = \tempnam(\sys_get_temp_dir(), "fpp");
 
-        if (($fileSignedPath === false) || ($filePlainPath === false)) {
-            throw new EFattureWsClientException("Unable to create temporary files.");
-        }
+            if (($fileSignedPath === false) || ($filePlainPath === false)) {
+                throw new EFattureWsClientException("Unable to create temporary files.");
+            }
 
-        $res = \file_put_contents($fileSignedPath, $string);
-        if ($res === false) {
-            throw new EFattureWsClientException("Unable to write temporary files.");
-        }
+            $res = \file_put_contents($fileSignedPath, $string);
+            if ($res === false) {
+                throw new EFattureWsClientException("Unable to write temporary files.");
+            }
 
-        if ($signingMethod === "CAdES-BES") {
             $openSslCommand = "openssl smime -verify -noverify -in '%s' -inform DER -out '%s'";
             $openSslCommand = \sprintf($openSslCommand, $fileSignedPath, $filePlainPath);
             $res = \exec($openSslCommand, $output);
+            $invoice->filePlainContent = \file_get_contents($filePlainPath);
+            unlink($filePlainPath);
+            unlink($fileSignedPath);
         }
 
-        if ($signingMethod === "XAdES-BES") {
-            $res = \file_put_contents($filePlainPath, $string);
+        if ($signingMethod === self::XAdES_BES) {
+            $invoice->filePlainContent = self::removeXadESBESSignature($string);
         }
 
         $invoice->fileSignedContent = $string;
         $invoice->fileSignedFingerprint = \md5($invoice->fileSignedContent);
 
-        $invoice->filePlainContent = \file_get_contents($filePlainPath);
         $invoice->filePlainFingerprint = \md5($invoice->filePlainContent);
 
         $invoice->signingMethod = $signingMethod;
-
-        unlink($fileSignedPath);
-        unlink($filePlainPath);
 
         if (empty($invoice->filePlainContent)) {
             throw new EFattureWsClientException("Openssl was unable to decrypt file.");
