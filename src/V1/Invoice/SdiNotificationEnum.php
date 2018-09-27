419f1bd3c0f0202643661b9d439db6133f26dcf6
Fabio Soggia <f.soggia@gmail.com>
2018-09-27 13:00:04 +0200

    Aggiunta classe SdiNotificationEnum.

---
+++ src/V1/Invoice/SdiNotificationEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace CloudFinance\EFattureWsClient\V1\Invoice;
+
+class SdiNotificationEnum {
+    public const RICEVUTA_CONSEGNA = "RicevutaConsegna";
+    public const NOTIFICA_MANCATA_CONSEGNA = "NotificaMancataConsegna";
+    public const NOTIFICA_SCARTO = "NotificaScarto";
+    public const NOTIFICA_ESITO = "NotificaEsito";
+    public const NOTIFICA_DECORRENZA_TERMINI = "NotificaDecorrenzaTermini";
+    public const ATTESTAZIONE_TRASMISSIONE_FATTURA = "AttestazioneTrasmissioneFattura";
+}
